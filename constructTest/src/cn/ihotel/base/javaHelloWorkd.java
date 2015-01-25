e05a9cb7e79afd24f2424097c5a40cece6257806
JavaProgrammerLB <javaprogrammerlb@hotmail.com>
2015-01-25 19:49:07 +0800

    一个HelloWorld程序

---
+++ constructTest/src/cn/ihotel/base/javaHelloWorkd.java
@@ -0,0 +1,13 @@
+package cn.ihotel.base;
+
+public class javaHelloWorkd {
+
+	/**
+	 * 这是一个Hello World程序！
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello World!!yuanmengchen");
+	}
+
+}
